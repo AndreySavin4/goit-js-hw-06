276ebc712c94ed64d79864ab788998be227fcd98
AndreySavin4 <andreysavin4@ukr.net>
2023-02-20 21:36:22 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -4,7 +4,5 @@ inputEl.addEventListener("input", (e) => {
   const spanEl = document.querySelector("#text");
   const fontSize = e.currentTarget.value;
 
-  e.currentTarget.value;
-
   spanEl.style.fontSize = `${fontSize}px`;
 });
